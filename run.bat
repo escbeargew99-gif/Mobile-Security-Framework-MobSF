21fdda892c40e8fe71396a208e278a16aeae31d1
BigBearHub29 <escbeargew99@gmail.com>
2026-02-08 17:30:47 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,4 +7,5 @@ goto :run
 SET conf="0.0.0.0:8000 [::]:8000"
 :run
 echo Running MobSF on %conf%
+
 poetry run waitress-serve --listen=%conf% --threads=10 --channel-timeout=3600 mobsf.MobSF.wsgi:application
